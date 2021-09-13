bc9e1391157c75b18eca2a28735dd8bf44a141f9
CmdBlock-zqg <zhuqinggangemail@qq.com>
2021-09-13 22:18:07 +0800

    update conf

--- conf.example.js
+++ conf.example.js
@@ -3,14 +3,22 @@ module.exports = {
         baseURL: 'http://127.0.0.1:5700/',
         listen: '5701'
     },
-    groups: [111111111, 22222222],
-    admin: 333333333,
+    id: 0000000000,
     weibo: {
+        broadcast: {
+            groups: [0000000000, 0000000000],
+            users: [0000000000]
+        },
         userIdList: ["6279793937", "6441489862", "7499841383"],
         cookie: "[微博cookie]"
     },
     intervals: {
         updateGameData: 6 * 3600 * 1000,
         updateWeibo: 180 * 1000
+    },
+    handlers: {
+        0: [], // 私聊
+        1083320850: ['common'],
+        907815252: ['common']
     }
 }
