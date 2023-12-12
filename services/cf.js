aeb1d944e7ee469b5a1175e96daf5f9d6718d46f
CmdBlock <CmdBlockZQG@outlook.com>
2023-12-13 03:41:06 +0800

    Update cf.js

--- services/cf.js
+++ services/cf.js
@@ -10,10 +10,12 @@ const groups = conf.services.cf.groups;
 
 let handleGroups = {};
 let lstTouched = {};
+let handleTesting = {};
 let timer = false;
 
 const statusDesc = (s) => {
   const map = {
+    "PARTIAL": "对了却没有完全对",
     "COMPILATION_ERROR": "编译没过，他在干什么",
     "WRONG_ANSWER": "WA的一声哭了出来",
     "PRESENTATION_ERROR": "输出格式错误哈哈哈哈哈",
@@ -25,9 +27,59 @@ const statusDesc = (s) => {
   return map[s] ? map[s] : "没过";
 };
 
+const tick = async () => {
+  for (const handle of Object.keys(handleGroups)) {
+    const t = now();
+    let status;
+    try {
+      const { data } = await axios.get(`https://codeforces.com/api/user.status?handle=${handle}&from=1&count=20`);
+      status = data.result.reverse();
+    } catch {
+      continue;
+    }
+    // console.log(status);
+
+    const send = (msg) => {
+      for (const group of handleGroups[handle]) {
+        sendGroupMsg(group, msg);
+      }
+    }
+
+    if (!handleTesting[handle]) handleTesting[handle] = {};
+
+    if (lstTouched[handle] < 0) {
+      status = status.filter(x => x.creationTimeSeconds > -lstTouched[handle]);
+    } else {
+      status = status.filter(x => x.id > lstTouched[handle] || handleTesting[handle][x.id]);
+    }
+    for (const x of status) {
+      if (x.id > lstTouched[handle]) lstTouched[handle] = x.id;
+      
+      if (!x.verdict || x.verdict === "TESTING") {
+        handleTesting[handle][x.id] = true;
+        continue;
+      }
+      if (handleTesting[handle][x.id]) {
+        delete handleTesting[handle][x.id];
+      }
+
+      const prob = `${x.problem.contestId}${x.problem.index} - ${x.problem.name}`;
+      let msg = "";
+      if (x.verdict === "OK") {
+        msg = `${handle}提交并通过了${prob}！`
+      } else {
+        msg = `${handle}提交了${prob}，然而${statusDesc(x.verdict)}`
+      }
+      send(msg);
+    }
+    await sleep(5000);
+  }
+};
+
 async function init() {
   // console.log("init");
   handleGroups = {};
+  handleTesting = {};
   const t = now();
   for (const group of groups) {
     const db = new StorageProvider("cf_" + group);
@@ -39,53 +91,11 @@ async function init() {
     }
   }
   if (timer) clearInterval(timer);
-  timer = setInterval(async () => {
-    // console.log("timer tick");
-    const t = now();
-    const handles = Object.keys(handleGroups).join(";");
-    let userList;
-    try {
-      const { data } = await axios.get(`https://codeforces.com/api/user.info?handles=${handles}`);
-      userList = data.result;
-      // console.log(userList);
-    } catch {
-      return;
-    }
-    for (const user of userList) {
-      if (t - user.lastOnlineTimeSeconds > 30 * 60) continue;
-      
-      let status;
-      try {
-        const { data } = await axios.get(`https://codeforces.com/api/user.status?handle=${user}&from=1&count=20`);
-        status = data.result;
-      } catch {
-        continue;
-      }
-      // console.log(status);
-
-      const send = (msg) => {
-        for (const group of handleGroups[user]) {
-          sendGroupMsg(group, msg);
-        }
-      }
-
-      status.filter(x => x.creationTimeSeconds > t && x.verdict && x.verdict !== "PARTIAL" && x.verdict !== "TESTING");
-      for (const x of status) {
-        const prob = `${x.problem.contestId}${x.problem.index} - ${x.problem.name}`;
-        let msg = "";
-        if (x.verdict === "OK") {
-          msg = `${user}提交并通过了${prob}！`
-        } else {
-          msg = `${user}提交了${prob}，然而${statusDesc(x.verdict)}`
-        }
-        send(msg);
-      }
-      await sleep(5000);
-    }
-  }, 60 * 3 * 1000);
+  
+  timer = setInterval(tick, 60 * 3 * 1000);
 }
 
-init();
+init().then(tick);
 
 module.exports = {
   reloadService: init
